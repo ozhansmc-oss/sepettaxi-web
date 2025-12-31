abac1bd326fa7218d8eb927997847dfe8bb55aa0
ozhansmc-oss <ozhansmc@icloud.com>
2025-12-31 11:37:46 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// SepetTaxi – Global Config
+window.SEPT = {
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzzwdIUkPheKBoJS3U13pvrFMi9VutO-M0gM-LRfDnUnrk6JwrBAevMKp9a4Ecy6Dcy/exec",
+  COMMISSION: 0.15,
+  POLL_MS: 3000
+};
