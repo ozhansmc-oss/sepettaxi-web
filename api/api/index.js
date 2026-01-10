05cec6ac805e6751321dc7c216961f573226a0ef
ozhansmc-oss <ozhansmc@icloud.com>
2026-01-10 16:13:25 +0300

    Update index.js

--- api/api/index.js
+++ api/api/index.js
@@ -1,7 +1,3 @@
-export const config = {
-  runtime: "nodejs18.x"
-};
-
 export default async function handler(req, res) {
   const GAS_URL = "https://script.google.com/macros/s/AKfycbxxvr6OPG5R-emW8WlquzZ1psFEn6Lbvt4dDHpdN8XWO494N7uCv3s4DhLreIhO59b-/exec";
 
@@ -12,7 +8,7 @@ export default async function handler(req, res) {
       body = JSON.stringify(req.body || {});
     }
 
-    const response = await fetch(GAS_URL, {
+    const r = await fetch(GAS_URL, {
       method: req.method,
       headers: {
         "Content-Type": "text/plain;charset=utf-8"
@@ -20,16 +16,13 @@ export default async function handler(req, res) {
       body
     });
 
-    const text = await response.text();
+    const text = await r.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "application/json");
     res.status(200).send(text);
 
-  } catch (err) {
-    res.status(500).json({
-      ok: false,
-      error: String(err)
-    });
+  } catch (e) {
+    res.status(500).json({ ok: false, error: String(e) });
   }
 }
